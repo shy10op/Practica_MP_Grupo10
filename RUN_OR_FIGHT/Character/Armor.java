d2bfd339fb50e5d50a1f5e59178044e5fcd3caad
Christopher-SV <stevenvinue@gmail.com>
2024-04-10 23:29:45 +0200

    Classes: Weapon & Armor

--- RUN_OR_FIGHT/Character/Armor.java
+++ RUN_OR_FIGHT/Character/Armor.java
@@ -1,5 +1,46 @@
 package Character;
 
 public class Armor {
-    
+    private String name;
+    private int modAttack;
+    private int modDefense;
+
+    public Armor() {
+    }
+
+    public Armor(String name, int modDefense) {
+        this.name = name;
+        this.modDefense = modDefense;
+    }
+
+    public Armor(String name, int modAttack, int modDefense) {
+        this.name = name;
+        this.modAttack = modAttack;
+        this.modDefense = modDefense;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getModAttack() {
+        return modAttack;
+    }
+
+    public void setModAttack(int modAttack) {
+        this.modAttack = modAttack;
+    }
+
+    public int getModDefense() {
+        return modDefense;
+    }
+
+    public void setModDefense(int modDefense) {
+        this.modDefense = modDefense;
+    }
+
 }
--- RUN_OR_FIGHT/Character/Weapon.java
+++ RUN_OR_FIGHT/Character/Weapon.java
@@ -1,5 +1,60 @@
 package Character;
 
 public class Weapon {
-    
+
+    private String name;
+    private int modAttack;
+    private int modDefense;
+    private int spaceHand;
+
+    public Weapon() {
+    }
+
+    // Arma con modificador de ataque
+    public Weapon(String name, int modAttack, int spaceHand) {
+        this.name = name;
+        this.modAttack = modAttack;
+        this.spaceHand = spaceHand;
+    }
+
+    // Arma con modificador de ataque y defensa
+    public Weapon(String name, int modAttack, int modDefense, int spaceHand) {
+        this.name = name;
+        this.modAttack = modAttack;
+        this.modDefense = modDefense;
+        this.spaceHand = spaceHand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getModAttack() {
+        return modAttack;
+    }
+
+    public void setModAttack(int modAttack) {
+        this.modAttack = modAttack;
+    }
+
+    public int getModDefense() {
+        return modDefense;
+    }
+
+    public void setModDefense(int modDefense) {
+        this.modDefense = modDefense;
+    }
+
+    public int getSpaceHand() {
+        return spaceHand;
+    }
+
+    public void setSpaceHand(int spaceHand) {
+        this.spaceHand = spaceHand;
+    }
+
 }
